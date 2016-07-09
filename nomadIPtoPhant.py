9b0ade6f874049cfc25768815d0f92db6e3064c7
Rustom Jehangir <rustyj110@gmail.com>
2016-07-09 16:51:13 -0700

    Add try/except for ip address getter.

--- nomadIPtoPhant.py
+++ nomadIPtoPhant.py
@@ -12,7 +12,12 @@ parser.add_option("-k","--key",dest="private_key",help="Phant private key for IP
 getURL = 'http://data.sparkfun.com/input/QGq9E8Yy1wc312pmoL0Z'
 
 while True:
-		ip = get('https://api.ipify.org').text 
+		ip = 'unknown'
+		try:
+			ip = get('https://api.ipify.org').text
+		except:
+			print "Unable to determine IP"
+			
 		print 'My public IP address is:', ip
 
 		print "Sending data to Sparkfun Phant..."
