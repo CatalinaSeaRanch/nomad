dcc302af1bce2f240e29ed8eee92936b3b4b2568
Rustom Jehangir <rustyj110@gmail.com>
2016-07-09 14:13:29 -0700

    Added initial script to send IP to Phant.

---
+++ nomadIPtoPhant.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+from optparse import OptionParser
+from requests import get 
+import requests
+import time
+
+parser = OptionParser()
+parser.add_option("-k","--key",dest="private_key",help="Phant private key for IP address data.")
+(options,args) = parser.parse_args()
+
+getURL = 'http://data.sparkfun.com/input/QGq9E8Yy1wc312pmoL0Z'
+
+while True:
+		ip = get('https://api.ipify.org').text 
+		print 'My public IP address is:', ip
+
+		print "Sending data to Sparkfun Phant..."
+		try: 
+				requestString = getURL+'?private_key='+options.private_key+'&ip_address='+ip
+				r = requests.get(requestString)
+				print "Request:"
+				print requestString
+				print "Result: "
+				print r.text
+		except ValueError:
+				print "Unexpected error:", sys.exc_info()[0]
+
+		time.sleep(60)
