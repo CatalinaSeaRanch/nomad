a967c13ac749541689f5da4f3f553af5f0b024ca
Tony White <vshierdom@gmail.com>
2016-07-09 22:36:26 -0700

    Initial commit of data collation script

---
+++ data_collection.py
@@ -0,0 +1,34 @@
+
+
+
+
+#To read ADC values - for battery voltage and current sense amplifier output - 
+#using Arduino, output via serial
+
+
+
+# To communicate I2C with Atlas Scientific Sensors - easiest to use A9
+# with drivers / methods from pi image hosted by Atlas - avoid arduino? Libraries may
+# be available for either
+
+
+
+# Vemco Acoustic Receiver - 96008N1 - Tag ID / TIME
+
+
+
+
+
+
+#Seabird MicroCAT - 96008n1 - Conducitivity / DO / Pressure / TEMP (?)
+
+
+
+
+
+#to read temp of NEO processor: cat /sys/class/thermal/thermal_zone0/temp
+# import commands
+#//need to replace print with casting to internal temp variable
+#print(commands.getstatusoutput("cat /sys/class/thermal/thermal_zone0/temp"))
+
+
