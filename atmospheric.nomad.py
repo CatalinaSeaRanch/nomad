ca27045dded0eb93ef13c83c742ea0df8a0f9047
Tony White <vshierdom@gmail.com>
2017-08-07 13:04:42 -0700

    met data parse start

--- atmospheric.nomad.py
+++ atmospheric.nomad.py
@@ -21,12 +21,34 @@ streamer = Streamer(bucket_name="NomadBuoy", bucket_key="DQQTCP4FWSYK", access_k
 
 #Seabird MicroCAT - 96008n1 - Conducitivity / DO / Pressure / TEMP / PSU
 ser = serial.Serial(
-    port='/dev/tty???',\
+    port='/dev/ttyUSB1',\
     baudrate=9600,\
     parity=serial.PARITY_NONE,\
     stopbits=serial.STOPBITS_ONE,\
     bytesize=serial.EIGHTBITS,\
     timeout=3)
 while True:
-	
+	time.sleep(1)
+	ser.write('?!')
+	time.sleep(0.5)
+	ser.write('0M1!')
+	time.sleep(0.5)
+	ser.write('0D0!'
+	input1 = ser.readline()
+	time.sleep(0.5)
+	ser.write('0M2!')
+	time.sleep(0.5)
+	ser.write('0D0!')
+	input2 = ser.readline()
+	print(input1)
+	print(input2)
+		
 
+		#streamer.log("Relative Wind Direction",RWD)
+		#streamer.log("Relative Wind Speed",RWS)
+		#streamer.log("BaroPressure",bpressure)
+		#streamer.log("Relative Humidity",RH)
+		#streamer.log("Air Temp",Atemp)
+		#streamer.log("Dewpoint",dpoint)
+
+		time.sleep(10)
